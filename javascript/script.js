0da354a2ec904b6d8c69cfb4f63356da59921895
hpreksha <preksha.hirani@marutitech.com>
2024-12-12 10:50:02 +0530

    build: set up folder structure for JavaScript files

--- javascript/script.js
+++ javascript/script.js
@@ -13,4 +13,34 @@ const arr = Object.values(obj);
 document.getElementById('object').innerHTML="Hello " + obj.fullName();
 var obj2 = new Object();
 obj2.fname = "John";
-document.getElementById('object2').innerHTML="Hello " + obj.fname;
+
+
+// const fruits = {Bananas:300, Oranges:200, Apples:500};
+
+// let text = "";
+// for (let [fruit, value] of Object.entries(fruits)) {
+//   text += fruit + ": " + value + "<br>";
+// }
+
+let x="";
+const obj3={
+  name:"John",
+  cars:[
+    {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+    {name:"BMW", models:["320", "X3", "X5"]}
+  ]
+}
+for (let i in obj3.cars) {
+  x+= "<h2>" + obj3.cars[i].name + "</h2>";
+  for(let j in obj3.cars[i].models) {
+    x+=obj3.cars[i].models[j]+"<br>";
+  }
+}
+document.getElementById('object-eg').innerHTML=x;
+
+
+const arr2=[1,2,3,4,5];
+document.getElementById("arr-eg").innerHTML="Element greater than 4 is " + arr2.find(func) + ".";
+function func(value) {
+  return value > 4;
+}
