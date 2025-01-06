2262b9384275333f18251b2e7009a2341ad39551
hpreksha <preksha.hirani@marutitech.com>
2025-01-06 10:00:04 +0530

    delete: red-el.directive.spec.ts.

--- 04.angular/ang-topics/src/app/compRel/parent/child2/child2.component.ts
+++ 04.angular/ang-topics/src/app/compRel/parent/child2/child2.component.ts
@@ -4,7 +4,6 @@ import { Component, Output, EventEmitter } from '@angular/core';
   selector: 'app-child2',
   imports: [],
   templateUrl: './child2.component.html',
-  styleUrl: './child2.component.css',
 })
 export class Child2Component {
   @Output() newMessage = new EventEmitter<string>();
@@ -13,5 +12,4 @@ export class Child2Component {
     // const message = 'Message from Child To Parent Component.';
     this.newMessage.emit(`Message from child2 component: ${message}`);
   }
-  
 }
