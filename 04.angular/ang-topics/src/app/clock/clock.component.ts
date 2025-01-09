7325759c6b320a9806593e20a6a5febbf44dc011
hpreksha <preksha.hirani@marutitech.com>
2025-01-09 12:22:24 +0530

    refactor: Changed some variable name in clock component.

--- 04.angular/ang-topics/src/app/clock/clock.component.ts
+++ 04.angular/ang-topics/src/app/clock/clock.component.ts
@@ -1,58 +1,60 @@
-import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
-  imports: [],
   templateUrl: './clock.component.html',
-  styleUrl: './clock.component.css'
+  styleUrls: ['./clock.component.css'],
 })
 export class ClockComponent {
-  l_time:any;
-  curr_time:any='';
-  curr_time2:any='';
-  interval_id:any='';
-  d_time:any;
-  constructor(private cdr: ChangeDetectorRef) {}
+  currentTime: Date | null = null;
+  intervalId: ReturnType<typeof setInterval>|null|undefined;
+  displayTime: string = '00:00:00';
+
   ngOnInit() {
-    console.log("ngOnInit method.");
+    console.log('ngOnInit method.');
   }
 
-
   ngAfterViewInit() {
-    console.log("AfterView method.");
-    this.l_time = localStorage.getItem("s_time");
-    if(!this.l_time) {
-      this.l_time = new Date();
-      this.l_time = this.l_time.toString();
-      localStorage.setItem("s_time", this.l_time);
+    console.log('AfterView method.');
+    const storedTime = localStorage.getItem('setTime');
+
+    if(storedTime) {
+      this.currentTime = new Date(storedTime);
     }
-    this.curr_time = Date.parse(this.l_time);
-    this.curr_time2 = new Date(this.curr_time);
-    console.log(this.curr_time2.toLocaleTimeString()); 
-
-    this.interval_id = setInterval(()=> {  
-      console.log('si');
-      this.curr_time2 = new Date(this.curr_time2.getTime() + 1000);
-      this.d_time = this.curr_time2.toLocaleTimeString();
-      this.cdr.detectChanges();
-    },1000);
-  } 
-
+    else {
+      this.currentTime = new Date();
+      localStorage.setItem('setTime', this.currentTime.toString());
+    }
+    console.log('Start at: ', this.currentTime.toLocaleTimeString());
+    this.intervalId = setInterval(() => {
+      if (this.currentTime instanceof Date) {
+        this.currentTime = new Date(this.currentTime.getTime() + 1000);
+        this.displayTime = this.currentTime.toLocaleTimeString();
+      }
+    }, 1000);
+  }
 
   ngOnDestroy() {
-    if(this.interval_id) {
-      clearInterval(this.interval_id);
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
-    localStorage.setItem("s_time", this.curr_time2.toString());
-    console.log("Destroy method.");
+
+    if (this.currentTime instanceof Date) {
+      localStorage.setItem('setTime', this.currentTime.toString());
+    }
+    console.log('Exit at: ', this.currentTime);
+    console.log('Destroy method.');
   }
 
   @HostListener('window:beforeunload', ['$event'])
   func(event: BeforeUnloadEvent) {
-    if(this.interval_id) {
-      clearInterval(this.interval_id);
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
-    localStorage.setItem("s_time", this.curr_time2.toString());
-    console.log("Before unload method.");
+
+    if (this.currentTime instanceof Date) {
+      localStorage.setItem('setTime', this.currentTime.toString());
+    }
+    console.log('Before unload method.');
   }
 }
