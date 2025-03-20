547c16662a55386de04a00907b679205a7dec9c2
hpreksha <preksha.hirani@marutitech.com>
2025-03-20 18:32:57 +0530

    feat: react folder structure

---
+++ 06.react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Student() {
+  return (
+    <>
+    <h1>Hello</h1>
+    </>
+  );
+} 
+
+function App() {
+
+  return (
+    <>
+      <Student />
+      <Student />
+
+    </>
+  )
+}
+
+export default App
